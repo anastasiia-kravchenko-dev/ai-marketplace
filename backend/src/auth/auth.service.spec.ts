c562f801f9e03791b905efcc1c252f0f0f48d6bd
Anastasiia Kravchenko <anastasiia.kravchenko@coaxsoft.com>
2026-08-16 14:15:14 +0300

    fix: fix tests

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,4 +1,7 @@
+import { jest } from '@jest/globals'; // <-- Додаємо цей імпорт
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service.js';
 import { AuthService } from './auth.service.js';
 
 describe('AuthService', () => {
@@ -6,7 +9,23 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findByEmail: jest.fn(),
+            createUser: jest.fn(),
+            getUser: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
